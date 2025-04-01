2967dbe72e6ae130aa2d148ffc8d1a38800f3a59
🚀 <clapie.florent@gmail.com>
2025-04-01 10:25:47 +0200

    add colors

---
+++ src/app/tp2/display-yolo/display-yolo.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-display-yolo',
+  imports: [CommonModule],
+  templateUrl: './display-yolo.component.html',
+  styleUrl: './display-yolo.component.scss',
+})
+export class DisplayYoloComponent {
+  @Input() counter: number = 0;
+
+  getArray(counter: number) {
+    return new Array(counter).fill(0);
+  }
+
+  getColor(counter: number, reverse: boolean) {
+    // return counter % 2 === 0 ? 'blue' : 'red';
+    //  arc en ciel
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    if (reverse) {
+      return colors[colors.length - 1 - (counter % colors.length)];
+    }
+
+    return colors[counter % colors.length];
+  }
+}
--- src/app/tp2/parent/parent.component.ts
+++ src/app/tp2/parent/parent.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { DisplayYoloComponent } from '../display-yolo/display-yolo.component';
 
 @Component({
   selector: 'app-tp2-parent',
-  imports: [],
+  imports: [DisplayYoloComponent],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.scss',
 })
-export class ParentComponent {}
+export class ParentComponent {
+  counter = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+  reset() {
+    this.counter = 0;
+  }
+}
